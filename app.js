57c4161ba54d236d70173c3266caf94883d92f1d
piyushshri01 <piyushshrimal94@gmail.com>
2023-02-16 00:26:49 +0530

    initial

---
+++ app.js
@@ -0,0 +1,51 @@
+import express from "express";
+import { config } from "dotenv";
+import ErrorMiddleware from "./middlewares/Error.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+config({
+  path: "./config/config.env",
+});
+const app = express();
+
+// Using Middlewares
+// The main objective of this method is to parse the incoming request with urlencoded payloads and is based upon the body-parser.
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// for access cookies
+app.use(cookieParser());
+// cors for making intraction b/w server and frontend
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+// Importing & Using Routes
+import course from "./routes/courseRoutes.js";
+import user from "./routes/userRoutes.js";
+import payment from "./routes/paymentRoutes.js";
+import other from "./routes/otherRoutes.js";
+
+app.use("/api/v1", course);
+app.use("/api/v1", user);
+app.use("/api/v1", payment);
+app.use("/api/v1", other);
+
+export default app;
+
+app.get("/", (req, res) =>
+  res.send(
+    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+  )
+);
+
+app.use(ErrorMiddleware);
